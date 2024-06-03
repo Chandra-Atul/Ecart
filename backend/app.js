9ebe6e85cf60be663c34040877dc222c27812be1
Atul Chandra <atulchandra021@gmail.com>
2024-06-03 18:34:24 +0530

    frontend url added

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,16 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://eshop-cfl.vercel.app/",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 

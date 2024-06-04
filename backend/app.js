f4118ee97bf0a35c95693414b5b9da7329c8f2f8
Atul Chandra <atulchandra021@gmail.com>
2024-06-04 17:29:06 +0530

    new deployed backend url

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.use(
   })
 );
 
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "https://eshop-kappa-blond.vercel.app/api/v2";
-export const backend_url = "https://eshop-kappa-blond.vercel.app/";
+export const server = "https://ecart-backend-xi.vercel.app/api/v2";
+export const backend_url = "https://ecart-backend-xi.vercel.app/";

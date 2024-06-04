9a5991b48a225546d89de97e50ccd8824dc0c1fc
Atul Chandra <atulchandra021@gmail.com>
2024-06-04 14:14:07 +0530

    backend url updated again

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
 export const server =
-  "https://eshop-nm5swlbl3-atul-chandras-projects.vercel.app/api/v2";
+  "https://eshop-atul-chandras-projects.vercel.app/api/v2";
 export const backend_url =
-  "https://eshop-nm5swlbl3-atul-chandras-projects.vercel.app/";
+  "https://eshop-atul-chandras-projects.vercel.app/";
